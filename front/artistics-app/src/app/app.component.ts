ae88e404ebf29bf56188f56e255b343f05851640
adriellyb <adrielly0195@gmail.com>
2020-08-15 16:51:14 -0300

    TelaInicial+CadastroUsuario+Services

--- front/artistics-app/src/app/app.component.ts
+++ front/artistics-app/src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent implements OnInit {
     {
       title: 'My Profile',
       url: '/profile',
+      icon: 'person'
     },
 
     {
---
+++ front/artistics-app/src/app/services/register.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+  apiURL: string = 'http://localhost:8000/api/'
+
+  constructor(public http: HttpClient) { }
+
+  createUser(form):Observable<any> {
+    return this.http.post( this.apiURL + 'createUser', form);
+  }
+}
